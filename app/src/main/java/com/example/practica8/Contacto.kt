3bb6d2f7c397240af73c8dc682c894d03164f0ca
Oscar R <oscar.riv1590@gmail.com>
2024-03-20 18:41:30 -0600

    Initial commit

---
+++ app/src/main/java/com/example/practica8/Contacto.kt
@@ -0,0 +1,3 @@
+package com.example.practica8
+
+data class Contacto(val nombre: String, val telefono:String)
---
+++ app/src/main/java/com/example/practica8/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.practica8
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var rcv:RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        rcv = findViewById(R.id.rvContactos)
+    }//onCreate
+
+    override fun onResume() {
+        super.onResume()
+        Log.w("Contactos", "Hay ${ProvisionalDatos.listaContactos.size} contactos")
+        rcv.adapter = Adaptador()
+        rcv.layoutManager = LinearLayoutManager(this)
+    }
+
+    fun btnAgregar(v: View){
+        val intent = Intent(this, AgregarActivity::class.java)
+        startActivity(intent)
+    }
+}
